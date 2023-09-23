d74e3cec25c116c45d9177966db03bef3678c945
B1rzhan07 <birzhan.zhunusbekov@nu.edu.kz>
2023-09-24 01:24:28 +0600

    webcam

--- src/components/SOS/Webcam/Webcam.tsx
+++ src/components/SOS/Webcam/Webcam.tsx
@@ -5,7 +5,10 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React, { useRef, useState } from 'react';
 
-import { Button, TextField } from '@mui/material';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import {
+  Button, Dialog, DialogTitle, TextField, DialogContent, DialogActions,
+} from '@mui/material';
 import Webcam from 'react-webcam';
 import styled from 'styled-components';
 
@@ -25,21 +28,34 @@ function PhotoCapture() {
   const [email, setEmail] = useState<string>('');
   const [captureStatus, setCaptureStatus] = useState<string | null>(null);
   const [imageFile, setImageFile] = useState<File | null>(null);
-  const text = `Jerry, Astana, ${new Date().toLocaleDateString()}`;
-  const [takingPhoto, setTakingPhoto] = useState<boolean>(false);
+  const text2 = 'J E R R Y 💞';
+  const text = ` Astana,  ${new Date().getFullYear()}`;
+
+  // State for dialog open/close
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setDialogOpen(true);
+  };
+
+  const handleClose = () => {
+    setDialogOpen(false);
+  };
+
   const capturePhoto = () => {
     if (!webcamRef.current) return;
 
     const canvas = document.createElement('canvas');
-    canvas.width = webcamRef.current.video!.videoWidth;
-    canvas.height = webcamRef.current.video!.videoHeight;
+    canvas.width = webcamRef.current.video!.videoWidth * 2;
+    canvas.height = webcamRef.current.video!.videoHeight * 2;
     const context = canvas.getContext('2d')!;
 
     context.fillStyle = 'black';
+
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     const frame = new Image();
-    frame.src = '/frame.png';
+    frame.src = '/go.png';
 
     frame.onload = () => {
       context.drawImage(frame, 0, 0, canvas.width, canvas.height);
@@ -50,7 +66,7 @@ function PhotoCapture() {
       const paddingY = 20;
       const webcamWidth = canvas.width - 2 * paddingX;
       const webcamHeight = canvas.height - 2 * paddingY;
-
+      context.imageSmoothingEnabled = true;
       context.drawImage(
         webcamRef.current.video!,
         paddingX,
@@ -59,9 +75,12 @@ function PhotoCapture() {
         webcamHeight,
       );
 
-      context.font = '24px Roboto';
-      context.fillStyle = 'white';
-      context.fillText(text, 40, canvas.height - 30);
+      context.font = '72px Pacifico, cursive ';
+      context.fillStyle = 'pink';
+      context.fillText(text2, 50, canvas.height - 50);
+      context.font = '36px Pacifico, cursive ';
+
+      context.fillText(text, 50, canvas.height - 115);
       const capturedImage = canvas.toDataURL('image/jpeg');
       const blob = dataURItoBlob(capturedImage);
       const file = new File([blob], 'captured-image.jpg', { type: 'image/jpeg' });
@@ -69,8 +88,9 @@ function PhotoCapture() {
       setCaptureStatus('Photo captured successfully!');
     };
   };
+
   const handleCancel = () => {
-    setTakingPhoto(false);
+    setDialogOpen(false);
     setImage(null);
     setEmail('');
     setCaptureStatus(null);
@@ -93,12 +113,12 @@ function PhotoCapture() {
     })
       .then((response) => {
         console.log('Image sent successfully');
+        setDialogOpen(false);
       })
       .catch((error) => {
         console.error('Error sending image:', error);
       });
   };
-  console.log(imageFile);
 
   function dataURItoBlob(dataURI: string) {
     const byteString = atob(dataURI.split(',')[1]);
@@ -111,87 +131,73 @@ function PhotoCapture() {
     const blob = new Blob([ab], { type: mimeString });
     return blob;
   }
-  console.log(imageFile);
 
   return (
     <StyledPhotoContainer>
-      {!takingPhoto ? (
-        <Button color="primary" variant="outlined" type="button" onClick={() => setTakingPhoto(true)}>Take a Photo</Button>
-      ) : (
-        <>
-          <Webcam
-            audio={false}
-            ref={webcamRef}
-          />
-          <div style={{
-            display: 'flex',
-            marginTop: '10px',
-            marginBottom: '10px',
-            justifyContent: 'space-between',
-          }}
-          >
-            <Button
-              sx={{
-                marginRight: '10px',
+      <Button color="primary" variant="outlined" type="button" onClick={handleClickOpen}>
+        Take a Photo
+      </Button>
 
-              }}
-              color="primary"
-              variant="outlined"
-              type="button"
-              onClick={capturePhoto}
-            >
-              Capture Photo
+      {/* Dialog */}
+      <Dialog onClose={handleClose} open={dialogOpen} maxWidth="lg">
+        <DialogTitle>Capture Photo</DialogTitle>
+        <DialogContent>
+          <Webcam audio={false} ref={webcamRef} />
+        </DialogContent>
+        <DialogContent sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+        >
 
-            </Button>
-            <Button color="primary" variant="outlined" type="button" onClick={handleCancel}>Cancel</Button>
-          </div>
-          {image && (
-            <StyledCanvas>
-              <img src={image} alt="Captured" />
-            </StyledCanvas>
-          )}
           <TextField
             sx={{
-              marginBottom: '10px',
               width: '400px',
+              textAlign: 'center',
             }}
             type="email"
             placeholder="Enter email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-          <div style={{
-            display: 'flex',
-            marginTop: '10px',
-            marginBottom: '10px',
-          }}
+        </DialogContent>
+        <DialogActions sx={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+        >
+          <Button color="primary" variant="outlined" type="button" onClick={capturePhoto} sx={{}}>
+
+            <CameraAltIcon />
+          </Button>
+          <Button color="primary" variant="outlined" type="button" onClick={handleCancel}>
+            Cancel
+          </Button>
+          {imageFile && (
+          <Button color="primary" variant="outlined" type="button" onClick={handleSubmit}>
+            Submit
+          </Button>
+          )}
+          <Button
+            color="primary"
+            variant="outlined"
+            type="button"
+            onClick={() => {
+              if (imageFile) {
+                const blobUrl = URL.createObjectURL(imageFile);
+                const a = document.createElement('a');
+                a.href = blobUrl;
+                a.download = 'image.jpg';
+                a.click();
+                URL.revokeObjectURL(blobUrl);
+              }
+            }}
           >
-            <Button
-              type="button"
-              color="primary"
-              sx={{
-                marginRight: '10px',
-              }}
-              variant="outlined"
-              onClick={() => {
-                if (imageFile) {
-                  const blobUrl = URL.createObjectURL(imageFile);
-                  const a = document.createElement('a');
-                  a.href = blobUrl;
-                  a.download = 'image.jpg';
-                  a.click();
-                  URL.revokeObjectURL(blobUrl);
-                }
-              }}
-            >
-              Download
-            </Button>
-            {imageFile && (
-            <Button color="primary" variant="outlined" type="button" onClick={handleSubmit}>Submit</Button>
-            )}
-          </div>
-        </>
-      )}
+            Download
+          </Button>
+        </DialogActions>
+      </Dialog>
     </StyledPhotoContainer>
   );
 }
