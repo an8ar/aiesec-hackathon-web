7b3308ca5a24bdefeedbdca53e170c4548e700cf
Ansar <annssarr@gmail.com>
2023-09-24 07:37:41 +0600

    add: env variables

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,2 +1,4 @@
 export const API_URL = import.meta.env.VITE_API_URL;
 export const APPLICATION_NAME = 'JERRY';
+export const { GOOGLE_MAPS_API_KEY } = import.meta.env;
+export const { CHAT_GPT_API_KEY } = import.meta.env;
